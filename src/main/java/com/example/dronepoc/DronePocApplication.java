dd79b14d77bb80f447a7cf5d4399aa6a4ab8c706
Dheeraj Kumar <dheeraj.kumar@alphaitsystems.com>
2021-04-26 13:44:23 +0530

    Project setup complete

---
+++ src/main/java/com/example/dronepoc/DronePocApplication.java
@@ -0,0 +1,13 @@
+package com.example.dronepoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DronePocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DronePocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dronepoc/DronePocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dronepoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DronePocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

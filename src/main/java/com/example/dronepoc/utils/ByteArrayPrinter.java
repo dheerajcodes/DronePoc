1aa9531c48efe73d8d3d5bbe3639e81e876af5ac
Dheeraj Kumar <dheeraj.kumar@alphaitsystems.com>
2021-04-28 15:54:57 +0530

    Refactoring

--- src/main/java/com/example/dronepoc/utils/ByteArrayPrinter.java
+++ src/main/java/com/example/dronepoc/utils/ByteArrayPrinter.java
@@ -5,9 +5,9 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintWriter;
 
 public class ByteArrayPrinter {
-    private ByteArrayOutputStream baos = new ByteArrayOutputStream();
-    private PrintWriter pw = new PrintWriter(baos);
-    private ServletOutputStream sos = new ByteArrayServletStream(baos);
+    private final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    private final PrintWriter pw = new PrintWriter(baos);
+    private final ServletOutputStream sos = new ByteArrayServletStream(baos);
 
     public PrintWriter getWriter() {
         return pw;

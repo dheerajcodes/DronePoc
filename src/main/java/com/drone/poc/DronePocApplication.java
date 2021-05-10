8cb86bb847622e995faefd19d32a23e28fd160a1
Dheeraj Kumar <dheeraj.kumar@alphaitsystems.com>
2021-05-10 14:19:37 +0530

    Refactored

--- src/main/java/com/drone/poc/DronePocApplication.java
+++ src/main/java/com/drone/poc/DronePocApplication.java
@@ -1,6 +1,8 @@
 package com.drone.poc;
 
 import org.h2.tools.Server;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +13,7 @@ import java.sql.SQLException;
 
 @SpringBootApplication
 public class DronePocApplication {
+    Logger logger = LoggerFactory.getLogger(DronePocApplication.class);
     @Value("${h2.server.tcp.port:9090}")
     private int h2TcpPort;
     @Value("${h2.server.tcp.enabled:false}")
@@ -23,9 +26,14 @@ public class DronePocApplication {
     }
 
     @PostConstruct
-    public void startH2DatabaseTcpServer() throws SQLException {
+    public void startH2DatabaseTcpServer() {
         if (tcpServerEnabled) {
-            this.h2DatabaseTcpServer = Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", String.valueOf(h2TcpPort)).start();
+            try {
+                this.h2DatabaseTcpServer = Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", String.valueOf(h2TcpPort)).start();
+                logger.info("Running H2 database server at : " + this.h2DatabaseTcpServer.getURL());
+            } catch (SQLException exception) {
+                logger.error("Failed to start H2 database server - " + exception.getMessage());
+            }
         }
     }
 
